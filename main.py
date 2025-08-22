064015299a1de19a6ac8aad740d379061da0536e
Nana Danquah <125459868+NanaDanquah@users.noreply.github.com>
2025-08-22 11:05:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import yagmail
 
-yag = yagmail.SMTP(user="pythonlesson25@gmail.com", password="prhjzbqxjoeaarjf")
+yag = yagmail.SMTP(user="pythonlesson25@gmail.com", password="************")
 
 yag.send(
-    to="danquahbadugabriel@gmail.com",
+    to="recevieremail@gmail.com",
     subject="Holiday Plans",
-    contents="Hello Gabriel, what are your plans for the upcoming holidays"
+    contents="Hello Gabriel, what are your plans for the upcoming holidays?"
 )
+
